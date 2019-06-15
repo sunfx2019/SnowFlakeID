e5f0f89340897edfbf06385160e0d57ad9c63894
sunfeixiang <123074528@qq.com>
2019-06-15 12:09:15 +0800

    更新
    Signed-off-by: sunfeixiang <123074528@qq.com>

---
+++ src/test/java/com/sunfx/test/T.java
@@ -0,0 +1,12 @@
+package com.sunfx.test;
+
+public class T {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(System.currentTimeMillis());
+ 
+	}
+
+}
